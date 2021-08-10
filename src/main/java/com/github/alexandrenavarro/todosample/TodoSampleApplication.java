9b9dd04ea5d8bd25fb0c2184fe601bb36b08da00
alexandre.navarro1 <alexandre.navarro@prismea.fr>
2021-08-11 01:36:39 +0200

    Add some config in pom.xml

--- src/main/java/com/github/alexandrenavarro/todosample/TodoSampleApplication.java
+++ src/main/java/com/github/alexandrenavarro/todosample/TodoSampleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//TODO REPLACE ME
 public class TodoSampleApplication {
 
 	public static void main(String[] args) {
